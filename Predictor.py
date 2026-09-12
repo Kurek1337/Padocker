de054055e6a2bf474cb4ba151332632a6d1f034b
Jakub Kurek <jakubk@tcd.ie>
2026-09-12 20:29:52 +0100

    Created F1-2023 CSV

--- Predictor.py
+++ Predictor.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd 
 from pprint import pprint 
+import time
 
 
 url = "https://api.openf1.org/v1/sessions?year=2023&session_name=Race"
@@ -14,110 +15,143 @@ data = response.json()
 if isinstance(data, list):
     sessions_df = pd.DataFrame(data)
 
-    session_key = sessions_df.iloc[0]["session_key"]
-    print("Selected session:", session_key)
+    all_races = []
+    for _, session in sessions_df.iterrows():
 
-    drivers_url = f"https://api.openf1.org/v1/drivers?session_key={session_key}"
-    drivers_response = requests.get(drivers_url, timeout=30)
-    drivers_data = drivers_response.json()
+        session_key = session["session_key"]
 
-    grid_url = f"https://api.openf1.org/v1/starting_grid?session_key={session_key}"
-    grid_response = requests.get(grid_url, timeout=30)
-    grid_data = grid_response.json()
+        print(
+        "Processing:",
+        session["circuit_short_name"],
+        session_key
+        )
 
-    results_url = f"https://api.openf1.org/v1/session_result?session_key={session_key}"
-    results_response = requests.get(results_url, timeout=30)
-    results_data = results_response.json()
+        if session["is_cancelled"]:
+            print("Skipping cancelled race")
+            continue
+        
+
+        
+        
+
+        
+        print("Selected session:", session_key)
+
+        ####################
+        #Drivers
+        ####################
+
+        drivers_url = f"https://api.openf1.org/v1/drivers?session_key={session_key}"
+        drivers_response = requests.get(drivers_url, timeout=30)
+        drivers_data = drivers_response.json()
+        time.sleep(2.1)
+        ####################
+        #Grid
+        ####################
+
+        grid_url = f"https://api.openf1.org/v1/starting_grid?session_key={session_key}"
+        grid_response = requests.get(grid_url, timeout=30)
+        grid_data = grid_response.json()
+        time.sleep(2.1)
+        ####################
+        #Results
+        ####################
+
+        results_url = f"https://api.openf1.org/v1/session_result?session_key={session_key}"
+        results_response = requests.get(results_url, timeout=30)
+        results_data = results_response.json()
+        time.sleep(2.1)
+        ####################
+        #Data Checks
+        ####################
+        print("Grid status code:", grid_response.status_code)
+
+        if isinstance(grid_data, list):
+            grid_df = pd.DataFrame(grid_data)
+
+            print(grid_df.head())
+            print(grid_df.columns)
+        else:
+            print("No starting grid data available:")
+            print(grid_data)
 
 
-    print("Grid status code:", grid_response.status_code)
+        if not isinstance(drivers_data, list):
+            print("No drivers available - skipping race")
+            continue
 
-    if isinstance(grid_data, list):
-        grid_df = pd.DataFrame(grid_data)
+        if not isinstance(results_data, list):
+            print("No results available - skipping race")
+            continue
 
-        print(grid_df.head())
-        print(grid_df.columns)
-
-    else:
-        print("No starting grid data available:")
-        print(grid_data)
-
-    if isinstance(drivers_data, list):
         drivers_df = pd.DataFrame(drivers_data)
-    else:
-        print("No drivers avaible")
 
-    if isinstance(results_data, list):
         results_df = pd.DataFrame(results_data)
-        results_df = results_df.rename(columns={"position": "finish_position"})
-        print("working")
-    else:
-        print("No results available")
+        results_df = results_df.rename(
+            columns={"position": "finish_position"}
+        )
 
-    #results_df = results_df.rename(columns={"position": "finish_position"})
-    print("Results here")
-    print(results_df.head())
-    print(results_df.columns)
-
-    print(sessions_df[["circuit_short_name", "date_start", "session_key"]])
-
-    print(sessions_df.head())
-    print(sessions_df.columns)
-
-    print (drivers_df.head())
-    print (drivers_df.columns)
-
-   
-    #df.to_csv("Sessions-2023.csv", index = False)
-    #print("CSV created successfully.")
+        ####################
+        #Merge
+        ####################
+        race_df = pd.merge(
+            drivers_df,
+            results_df,
+            on=["driver_number", "session_key", "meeting_key"]
+        )
+    
+        race_df = race_df[
+            [
+                "meeting_key",
+                "session_key",
+                "driver_number",
+                "full_name",
+                "team_name",
+                "finish_position",
+                "number_of_laps",
+                "points",
+                "dnf",
+                "dns",
+                "dsq"
+            ]
+        ]
+        ####################
+        #Data Checks
+        ####################
+        race_df["finished_in_points"] = (race_df["points"] > 0).astype(int)
+    
+        
+        print("merged here -----")
+        print(race_df.head())
+        print(race_df.columns)
+    
+    
+    
+        race_df["year"] = session["year"]
+        race_df["circuit_name"] = session["circuit_short_name"]
+        race_df["race_date"] = session["date_start"]
+        
+        
+        all_races.append(race_df)
+    
+    season_df = pd.concat(
+        all_races,
+        ignore_index=True
+    )
+    
+    print(season_df)
+    
+    season_df.to_csv(
+        "F1-2023.csv",
+        index=False
+    )
+    # PredictedPosition = 10 # Initial hard-coded value for tests 
+    # After qualifying, predict whether a driver will finish in points (TOP 10)
+    # driverFinishInPoints = 1 if PredictedPosition <= 10 else 0 # Binary Classification 
+        
+        
 else:
-    print("The API returned an error:")
-    print(data)
+        print("The API returned an error:")
+        print(data)
 
-
-race_df = pd.merge(
-    drivers_df,
-    results_df,
-    on=["driver_number", "session_key", "meeting_key"]
-)
-
-race_df = race_df[
-    [
-        "meeting_key",
-        "session_key",
-        "driver_number",
-        "full_name",
-        "team_name",
-        "finish_position",
-        "number_of_laps",
-        "points",
-        "dnf",
-        "dns",
-        "dsq"
-    ]
-]
-
-race_df["finished_in_points"] = (race_df["points"] > 0).astype(int)
-print("merged here -----")
-print(race_df.head())
-print(race_df.columns)
-
-selected_session = sessions_df[
-    sessions_df["session_key"] == session_key
-].iloc[0]
-
-race_df["year"] = selected_session["year"]
-race_df["circuit_name"] = selected_session["circuit_short_name"]
-race_df["race_date"] = selected_session["date_start"]
-
-
-
-print("FINAL RACE DATA:")
-print(race_df.head())
-print(race_df.columns)
-
-race_df.to_csv("Bahrain-2023.csv", index=False)
-# PredictedPosition = 10 # Initial hard-coded value for tests 
-# After qualifying, predict whether a driver will finish in points (TOP 10)
-# driverFinishInPoints = 1 if PredictedPosition <= 10 else 0 # Binary Classification 
-
+    
